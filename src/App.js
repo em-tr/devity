cdecb931e71e64f2602f22d88b3e436b81b0338a
emitr <emily.truong1@hs-augsburg.de>
2022-09-05 17:22:25 +0200

    added material design for react (mui)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>hello world!</h1>
+        <Button variant="contained">Hello World</Button>
       </header>
     </div>
   );
